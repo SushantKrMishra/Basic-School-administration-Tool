f657b0fda6af7c00a67ebb967ef9cd0b0fe0a62f
Sushant Kr Mishra <76252181+SushantKrMishra@users.noreply.github.com>
2020-12-29 17:04:42 +0530

    Update FirstProject.py

--- FirstProject.py
+++ FirstProject.py
@@ -1,4 +1,4 @@
-#project1: Basic School Admistration Tool
+#project1: Basic School administrationTool
 import csv
 def write_into_csv(info_list):
     with open('student_info.csv' , 'a', newline= '') as csv_file:
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     condition = True
     student_num = 1
     while condition:
-        student_info  = input("Enter some Student Information for Student {}in the following format(Name,Age,Contact No,Email Id) :  ".format(student_num))
+        student_info  = input("Enter some Student Information for Student {} in the following format(Name,Age,Contact No,Email Id) :  ".format(student_num))
         Student_info_list = student_info.split(',')
         print("\nThe Entered information is - \nName: {}\nAge: {}\nContact Number: {}\nEmail Id: {}".format(Student_info_list[0],Student_info_list[1],Student_info_list[2],Student_info_list[3]))
         
@@ -19,6 +19,7 @@ if __name__ == '__main__':
         choice_check=choice_check.casefold()
         if choice_check == "yes":
             write_into_csv(Student_info_list)
+            student_num = student_num + 1
             
             condition_check =  input("Enter Yes or No if you want to Enter a Information of a Student:   ")
             condition_check=condition_check.casefold()
